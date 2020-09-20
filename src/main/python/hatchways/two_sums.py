842023f9a9cc275a6fcfd0976b54c4e21b212bcf
Rajan Khullar <rkhullar03@gmail.com>
2020-09-20 18:52:45 -0400

    save

--- src/main/python/hatchways/two_sums.py
+++ src/main/python/hatchways/two_sums.py
@@ -2,6 +2,12 @@ from typing import List
 
 
 def has_pair_1(data: List[int], key: int) -> bool:
+    """
+    brute force approach
+    use nested for loop to traverse all possible pairs in the array
+    time complexity = O(N^2)
+    space complexity = O(1)
+    """
     n = len(data)
     for i in range(n):
         for j in range(n):
@@ -13,14 +19,24 @@ def has_pair_1(data: List[int], key: int) -> bool:
 
 
 def has_pair_2(data: List[int], key: int) -> bool:
+    """
+    time complexity = O(N)
+    space complexity = O(N)
+    """
     n = len(data)
+    # create hash map of offset with indices
     partial = {key - data[i]: i for i in range(n)}
-    matches = set(tuple(sorted([data[i], key - data[i]])) for i in range(n)
-                  if data[i] in partial and i != partial[data[i]])
+    matches = set()
+    for i in range(n):
+        # ensure pair is not duplicated item
+        if data[i] in partial and i != partial[data[i]]:
+            pair = tuple(sorted([data[i], key - data[i]]))
+            matches.add(pair)
     return len(matches) > 0
 
 
 def has_pair(data: List[int], key: int) -> bool:
+    """returns true if a pair of integers in the input array adds up to a certain target"""
     # return has_pair_1(data, key)
     return has_pair_2(data, key)
 
