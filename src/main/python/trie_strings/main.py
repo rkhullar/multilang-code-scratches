ce82e6badd9493a0546ad116742cae878c9ea357
Rajan Khullar <rkhullar03@gmail.com>
2021-07-05 11:21:09 -0400

    move data structure to new module

--- src/main/python/trie_strings/main.py
+++ src/main/python/trie_strings/main.py
@@ -1,60 +1,6 @@
-from dataclasses import dataclass
-from typing import Generic, Set, TypeVar, List, Optional, Iterator
+from typing import List
+from data import Trie, TrieNode
 
-T = TypeVar('T')
-
-
-@dataclass
-class TrieNode(Generic[T]):
-    data: T
-    children: Set['TrieNode[T]'] = None
-    # parent: 'TrieNode[T]' = None
-
-    def __hash__(self):
-        return hash(self.data)
-
-    def __contains__(self, child: T) -> bool:
-        return self[child] is not None
-
-    def __getitem__(self, child: T) -> Optional['TrieNode[T]']:
-        if self.children is not None:
-            for node in self.children:
-                if node.data == child:
-                    return node
-
-    def __setitem__(self, child: T, grandchild: Optional[T] = None) -> None:
-        if self.children is None:
-            self.children = set()
-
-        if node := self[child]:
-            node[grandchild] = None
-
-        else:
-            node = TrieNode(data=child)
-            self.children.add(node)
-            # node.parent = self
-
-            if grandchild is not None:
-                node[grandchild] = None
-
-
-@dataclass
-class Trie(Generic[T]):
-    root: TrieNode[T] = None
-
-    def traverse(self, mode: str = 'bfs') -> Iterator[T]:
-        if mode == 'bfs':
-            return self._traverse_bfs()
-        raise ValueError
-
-    def _traverse_bfs(self) -> Iterator[T]:
-        queue: List[TrieNode[T]] = list()
-        queue.append(self.root)
-        while len(queue) > 0:
-            node = queue.pop(0)
-            yield node.data
-            if node.children is not None:
-                queue.extend(node.children)
 
 '''
 def build_trie_1(words: List[str]):
