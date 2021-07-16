0bbee04bc479f7f5c50d2166f4138e691506e387
Rajan Khullar <rkhullar03@gmail.com>
2021-07-16 12:53:51 -0400

    first attempt

---
+++ src/main/python/problems/trapping-rain-water/table.py
@@ -0,0 +1,28 @@
+from typing import Generic, Iterator, List, Tuple, TypeVar
+from dataclasses import dataclass, field
+
+T = TypeVar('T')
+
+
+@dataclass()
+class Table(Generic[T]):
+    width: int
+    height: int
+    default: T = field(default=None, repr=False)
+    data: List[List[T]] = field(default=None, init=False)
+
+    def __post_init__(self):
+        self.data = [[self.default for _ in range(self.width)] for _ in range(self.height)]
+
+    def __getitem__(self, item: slice):
+        return self.data[item]
+
+    def traverse(self) -> Iterator[Tuple[int, int, T]]:
+        for row_idx in range(self.height):
+            for col_idx in range(self.width):
+                yield row_idx, col_idx, self[row_idx][col_idx]
+
+
+if __name__ == '__main__':
+    matrix: Table[int] = Table(width=4, height=3, default=0)
+    print(matrix)
