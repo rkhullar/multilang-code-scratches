c6a42756a43a6255c09c4789d459c28bc4a8c91c
Rajan Khullar <rkhullar03@gmail.com>
2022-01-21 00:19:52 -0500

    save

--- src/test/python/problems/odd_even_jump/test.py
+++ src/test/python/problems/odd_even_jump/test.py
@@ -8,8 +8,8 @@ from problems.odd_even_jump.v1 import Solution
 class SolutionTest(TestCase):
     @parameterized.expand([
         ([10, 13, 12, 14, 15], 2),
-        # ([2, 3, 1, 1, 4], 3),
-        # ([5, 1, 3, 4, 2], 3)
+        ([2, 3, 1, 1, 4], 3),
+        ([5, 1, 3, 4, 2], 3)
     ])
     def test_multi(self, arr: list[int], expected: int):
         actual = Solution().oddEvenJumps(arr)
