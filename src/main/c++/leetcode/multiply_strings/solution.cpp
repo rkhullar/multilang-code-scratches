3a63bbb2b09edad0665eae5bec2725c5f5865b88
Rajan Khullar <rkhullar03@gmail.com>
2023-04-13 17:58:32 -0400

    save

--- src/main/c++/leetcode/multiply_strings/solution.cpp
+++ src/main/c++/leetcode/multiply_strings/solution.cpp
@@ -1,6 +1,44 @@
+typedef unsigned char byte;
+
+class BigInteger {
+
+private:
+    byte *digits;
+
+public:
+    BigInteger(int size = 0);
+    int length();
+
+//    string to_string() {
+//        return "BI";// + size();
+//    }
+};
+
+BigInteger::BigInteger(int size) {
+    this->digits = new byte[size];
+}
+
+int BigInteger::length() {
+    return sizeof(this->digits);
+}
+
 class Solution {
 public:
     string multiply(string num1, string num2) {
         return "0";
     }
 };
+
+
+/*
+class BigInt {
+  final private byte[] digits;
+  Person(int size) {
+  this.digits = new byte[size];
+  }
+
+  int size() {
+  return this.digits.length();
+  }
+}
+*/
--- src/main/c++/leetcode/multiply_strings/test_solution.cpp
+++ src/main/c++/leetcode/multiply_strings/test_solution.cpp
@@ -1,10 +1,14 @@
 int main()
 {
-    string a = "123", b = "456";
-    cout << a << endl;
-    cout << b << endl;
-    Solution solution;
-    string result = solution.multiply(a, b);
-    cout << result << endl;
+//    string a = "123", b = "456";
+//    cout << a << endl;
+//    cout << b << endl;
+//    Solution solution;
+//    string result = solution.multiply(a, b);
+//    cout << result << endl;
+
+    BigInteger a = BigInteger(3);
+    cout << a.length() << endl;
+
     return 0;
 }
