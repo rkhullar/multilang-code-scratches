151d59957d091bc22ad46abe082e17fa1c9f448e
Rajan Khullar <rkhullar03@gmail.com>
2023-04-16 16:50:40 -0400

    save

--- src/main/c++/leetcode/multiply_strings/test_solution.cpp
+++ src/main/c++/leetcode/multiply_strings/test_solution.cpp
@@ -22,22 +22,31 @@ void test_vector() {
     }
 }
 
-int main(int argc, char *argv[]) {
-//    string a = "123", b = "456";
-//    cout << a << endl;
-//    cout << b << endl;
-//    Solution solution;
-//    string result = solution.multiply(a, b);
-//    cout << result << endl;
+void test_parse() {
+    BigInteger::pointer a = BigInteger::from_string("123");
+    cout << a->size() << endl;
+    cout << *a << endl;
+}
 
+void test_plus() {
     BigInteger::pointer a = BigInteger::from_string("9999");
     BigInteger::pointer b = BigInteger::from_string("1");
-//    cout << a->size() << endl;
-//    cout << *a << endl;
-
     BigInteger::pointer c = *a + *b;
     cout << *c << endl;
+}
 
+void test_solution() {
+    string a = "123", b = "456";
+    cout << a << endl;
+    cout << b << endl;
+    Solution solution;
+    string result = solution.multiply(a, b);
+    cout << result << endl;
+}
+
+int main(int argc, char *argv[]) {
+    // test_plus();
+    test_solution();
     return 0;
 }
 
