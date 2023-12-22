7623b5475f62c4290e92d29942828813f7bfa9b9
Rajan Khullar <rkhullar03@gmail.com>
2023-12-22 17:14:05 -0500

    save

--- src/main/go/projects/hello-world/v2/lambda_function.go
+++ src/main/go/projects/hello-world/v2/lambda_function.go
@@ -7,14 +7,14 @@ import (
 	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
 )
 
-var ginLambda *ginadapter.GinLambda
+var ginLambda *ginadapter.GinLambdaV2
 
 func init() {
 	router := CreateRouter()
-	ginLambda = ginadapter.New(router)
+	ginLambda = ginadapter.NewV2(router)
 }
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	return ginLambda.ProxyWithContext(ctx, req)
 }
 
