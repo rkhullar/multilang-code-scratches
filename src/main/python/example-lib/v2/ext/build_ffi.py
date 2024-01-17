83c9d1e717f7b04881b4a179c7c1e7754272c836
Rajan Khullar <rkhullar03@gmail.com>
2024-01-17 12:34:41 -0500

    save

--- src/main/python/example-lib/v2/ext/build_ffi.py
+++ src/main/python/example-lib/v2/ext/build_ffi.py
@@ -7,9 +7,11 @@ shared_object_path, header_path = path / f'{name}.so', path / f'{name}.h'
 
 ffi = FFI()
 
-# lib = ffi.dlopen(str(path))
-
 ffi.set_source(module_name='example', source=f'#include "{header_path}"', extra_objects=[str(shared_object_path)])
+ffi.cdef('''
+extern void Hello(char* message, int count);
+extern void Check();
+''')
 
 if __name__ == '__main__':
-    ffi.compile(verbose=True, tmpdir='out', target='pyexample.o')
+    ffi.compile(verbose=True, tmpdir='ext/out', target='pyexample')
--- src/main/python/example-lib/v2/test.py
+++ src/main/python/example-lib/v2/test.py
@@ -1,2 +1,9 @@
-def hello(message: str, count: int):
-    lib.Hello(ffi.new('char[]'), message.encode(), ffi.new('int', count))
+# lib = ffi.dlopen(str(path))
+
+# def hello(message: str, count: int):
+#     lib.Hello(ffi.new('char[]'), message.encode(), ffi.new('int', count))
+
+
+from example import lib, ffi
+
+print(lib)
