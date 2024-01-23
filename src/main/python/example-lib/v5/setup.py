a51f8f123a087619b5fde87656ce599574f0db4f
Rajan Khullar <rkhullar03@gmail.com>
2024-01-22 20:45:17 -0500

    save

--- src/main/python/example-lib/v5/setup.py
+++ src/main/python/example-lib/v5/setup.py
@@ -1,4 +1,26 @@
 from setuptools import setup, find_packages
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+from pathlib import Path
+import subprocess
+import sys
+
+
+def precompile():
+    path = Path(__file__).parent.absolute() / 'example'
+    subprocess.run('Make', cwd=path)
+
+
+def patch_wheel_darwin():
+    print('need to patch _example.abi3.so in wheel file')
+
+
+class CustomBuildWheel(_bdist_wheel):
+    def run(self):
+        precompile()
+        _bdist_wheel.run(self)
+        if sys.platform == 'darwin':
+            patch_wheel_darwin()
+
 
 setup(
     name='example-lib',
@@ -6,5 +28,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     cffi_modules=['example/build_ffi.py:builder'],
-    install_requires=['cffi']
+    install_requires=['cffi'],
+    cmdclass={'bdist_wheel': CustomBuildWheel}
 )
