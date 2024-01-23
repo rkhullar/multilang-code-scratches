afa9d4af0449a8a7fe6f6759e3f472d6e52f38c8
Rajan Khullar <rkhullar03@gmail.com>
2024-01-22 20:03:02 -0500

    save

--- src/main/python/example-lib/v5/example/build_ffi.py
+++ src/main/python/example-lib/v5/example/build_ffi.py
@@ -1,20 +1,32 @@
 from cffi import FFI
 from pathlib import Path
+import sys
 
 builder = FFI()
+
 
 path = Path(__file__).parent.absolute() / 'lib'
 header_path = path / 'libexample.h'
 shared_object_path = path / 'libexample.so'
+
+
+def build_extra_set_source_args() -> dict[str, list[str]]:
+    match sys.platform:
+        case 'linux':
+            set_rpath = ['-Wl,-rpath=$ORIGIN/example/lib']
+            return dict(extra_compile_arg=set_rpath, extra_link_args=set_rpath)
+        case 'darwin':
+            return dict()
+        case _:
+            return dict()
+
 
 builder.set_source(
     module_name='_example',
     source=f'#include "{header_path}"',
     libraries=['example'],
     library_dirs=[str(shared_object_path.parent)],
-    # extra_compile_args=['-Wl,-rpath,$ORIGIN'],
-    # extra_link_args=['-Wl,-rpath,$ORIGIN']
-    extra_link_args=['-Wl,-rpath,$ORIGIN/example/lib']
+    **build_extra_set_source_args()
 )
 
 builder.cdef('''
@@ -24,4 +36,21 @@ builder.cdef('''
 
 
 if __name__ == '__main__':
-    builder.compile(verbose=True)  # tmpdir='out'
+    builder.compile(verbose=True, tmpdir='out')  # tmpdir='out'
+
+# otool -L _example.abi3.so
+# install_name_tool -change libexample.so @loader_path/example/lib/libexample.so _example.abi3.so
+
+'''
+works on linux:
+set_rpath = ['-Wl,-rpath=$ORIGIN/example/lib']
+
+builder.set_source(
+    module_name='_example',
+    source=f'#include "{header_path}"',
+    libraries=['example'],
+    library_dirs=[str(shared_object_path.parent)],
+    extra_compile_args=set_rpath,
+    extra_link_args=set_rpath
+)
+'''
