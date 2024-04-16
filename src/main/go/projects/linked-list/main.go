9113510df4a847afe8c95e9898f7167316a742f7
Rajan Khullar <rkhullar03@gmail.com>
2024-04-15 23:35:47 -0400

    save

--- src/main/go/projects/linked-list/main.go
+++ src/main/go/projects/linked-list/main.go
@@ -8,6 +8,10 @@ type ListNode[T any] struct {
 	next *ListNode[T]
 }
 
+func NewListNode[T any](data T) *ListNode[T] {
+	return &ListNode[T]{data, nil, nil}
+}
+
 type List[T any] struct {
 	head *ListNode[T]
 	tail *ListNode[T]
@@ -23,11 +27,26 @@ func (this *List[T]) Push(data T) {
 }
 
 func (this *List[T]) String() string {
+	curr := this.head
+	for curr != nil {
+		fmt.Println(curr.data)
+		curr = curr.next
+	}
 	return "list format"
 }
 
 func main() {
 	dut := NewList[int]()
+	a := NewListNode(1)
+	b := NewListNode(2)
+	c := NewListNode(3)
+	a.next = b
+	b.prev = a
+	b.next = c
+	c.prev = b
+	dut.head = a
+	dut.tail = c
+	dut.size = 3
 	fmt.Println("Hello world")
 	fmt.Println(dut)
 	dut.Push(7)
